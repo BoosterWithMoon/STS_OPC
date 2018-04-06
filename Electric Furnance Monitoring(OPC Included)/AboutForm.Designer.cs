5045005e23ebf277f62e744b31b7acd8bcc84e3c
STKIM <darkman0212@naver.com>
2018-04-06 18:15:36 +0900

    180406 COMMIT CAM2_APPLIED

--- Electric Furnance Monitoring(OPC Included)/AboutForm.Designer.cs
+++ Electric Furnance Monitoring(OPC Included)/AboutForm.Designer.cs
@@ -61,7 +61,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("맑은 고딕", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label1.Location = new System.Drawing.Point(143, 26);
+            this.label1.Location = new System.Drawing.Point(143, 32);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(309, 25);
             this.label1.TabIndex = 2;
@@ -71,7 +71,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("맑은 고딕", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label2.Location = new System.Drawing.Point(144, 55);
+            this.label2.Location = new System.Drawing.Point(144, 61);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(228, 19);
             this.label2.TabIndex = 3;
@@ -80,11 +80,12 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(146, 114);
+            this.label3.Font = new System.Drawing.Font("맑은 고딕", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label3.Location = new System.Drawing.Point(143, 88);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(185, 12);
+            this.label3.Size = new System.Drawing.Size(292, 25);
             this.label3.TabIndex = 4;
-            this.label3.Text = "WONWOO SYSTEMS, CO,. LTD";
+            this.label3.Text = "WONWOO SYSTEMS, CO., LTD";
             // 
             // AboutForm
             // 
